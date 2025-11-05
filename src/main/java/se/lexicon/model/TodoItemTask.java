fe8c4f61f9e50e9f6c9091aab6f66aefc7f5a067
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-11-05 14:25:56 +0100

    Filled in TodoItemTask.java

--- src/main/java/se/lexicon/model/TodoItemTask.java
+++ src/main/java/se/lexicon/model/TodoItemTask.java
@@ -1,4 +1,54 @@
 package se.lexicon.model;
 
 public class TodoItemTask {
+    int id;
+    boolean assigned;
+    TodoItem todoItem;
+    Person assignee;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAssigned() {
+        return assigned;
+    }
+
+    public TodoItem getTodoItem() {
+        return todoItem;
+    }
+
+    public void setTodoItem(TodoItem todoItem) {
+        this.todoItem = todoItem;
+    }
+
+    public Person getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(Person assignee) {
+        this.assignee = assignee;
+        this.assigned= (assignee != null);
+    }
+
+    public TodoItemTask(Person assignee, TodoItem todoItem, int id) {
+        if (todoItem == null) {
+            throw new IllegalArgumentException("todoItem cannot be null");
+        }
+        this.assignee = assignee;
+        this.todoItem = todoItem;
+        this.id = id;
+        this.assigned = (assignee != null);
+    }
+
+    String assigneeName = (assignee != null)
+            ? assignee.getFirstName() + " " + assignee.getLastName()
+            : "none";
+
+    String title = (todoItem != null) ? todoItem.getTitle() : "null";
+
 }
